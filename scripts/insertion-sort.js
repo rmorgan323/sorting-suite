4261e5b9553e26df867559b2da5f1162429d723d
Rob Morgan <rmorgan323@gmail.com>
2017-10-16 19:29:11 -0600

    fixes git problem

---
+++ scripts/insertion-sort.js
@@ -0,0 +1,13 @@
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (array[j + 1] < array[j]) {
+        [array[j + 1], array[j]] = [array[j], array[j + 1]]
+      }
+    }
+  }
+
+  return array
+}
+
+// export insertionSort;
