ebe10e650f1c7f9da06aa892283cbb04a5bf8407
Rob Morgan <rmorgan323@gmail.com>
2017-10-16 19:14:54 -0600

    tweaks bubble sort, adds insertion sort and robs insertion sort

---
+++ tests/insertion-sort-test.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import '../scripts/insertion-sort.js';
+// import index from '../scripts/index'
+
+describe('Insertion Sort', () => {
+
+	it.skip('should correctly sort a small array', function() {
+	  
+	  let array = [5, 20, 3, 17, 7, 10, 1, 13, 9, 15, 18, 6];
+	  insertionSort(array);
+	  assert.equal(array, [1, 3, 5, 6, 7, 9, 10, 13, 15, 17, 18, 20])
+
+	});
+
+	it.skip('should correctly sort an array of up to 50000 values', function() {
+	  
+	  //create array
+	  let array = [];
+
+	  //populate array
+	  for (let i = 0; i < 50000; i++) {
+	  	let randomNum = Math.floor(Math.random() * 100000);
+	  	array.push(randomNum);
+	  }
+
+	  insertionSort(array);
+
+	});
+
+})
